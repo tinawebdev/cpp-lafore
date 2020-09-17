9b21aeab3bf3a1d88463b6009989086a1e612aa8
Kristina <codingkris@yahoo.com>
2020-09-17 19:44:57 +0300

    Completed exercise 03

---
+++ chapter02/ex03.cpp
@@ -0,0 +1,15 @@
+// Арифметическое присваивание и декремент
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val = 10;
+    cout << val << endl;
+    val+= 10;
+    cout << val << endl;
+    --val;
+    cout << val << endl;
+    return 0;
+}
