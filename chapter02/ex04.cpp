99065ce5e4335cf72fa9d26a83cb54f534350f76
Kristina <codingkris@yahoo.com>
2020-09-17 19:54:11 +0300

    Completed exercise 04

---
+++ chapter02/ex04.cpp
@@ -0,0 +1,12 @@
+// Программа, выводящая на экран стихотворение
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Roses are red\n"
+    << "Violets are blue\n"
+    << "Unexpected '{' on line 32" << endl;
+    return 0;
+}
