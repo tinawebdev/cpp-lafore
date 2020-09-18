c72556937ad4f38c7c0cefa5d1b941f68a973cdf
Kristina <codingkris@yahoo.com>
2020-09-18 14:33:05 +0300

    Fixed exercise 11

--- chapter02/ex11.cpp
+++ chapter02/ex11.cpp
@@ -7,15 +7,12 @@ int main()
     cout << setiosflags(ios::left) << setw(15) << "Last name" 
          << setw(10) << "Name" << setw(15) << "Adress" 
          << setw(13) << "City" << endl;
-    cout << setfill('-') << setw(53) << "" << endl;
-    cout << setfill(' ') << setw(15) << "Petrov" 
-         << setw(10) << "Vasiliy" << setw(15) << "Klenovaya 16" 
-         << setw(13) << "St.Petersburg" << endl;
-    cout << setiosflags(ios::left) << setw(15) << "Ivanov" 
-         << setw(10) << "Sergey" << setw(15) << "Osinovay 3" 
-         << setw(13) << "Nahodka" << endl;
-    cout << setiosflags(ios::left) << setw(15) << "Sidorov" 
-         << setw(10) << "Ivan" << setw(15) << "Berezovaya 21" 
-         << setw(13) << "Kaliningrad" << endl;
+    cout << setfill('-') << setw(53) << "" << setfill(' ') << endl;
+    cout << setw(15) << "Petrov" << setw(10) << "Vasiliy" << setw(15)
+         << "Klenovaya 16" << setw(13) << "St.Petersburg" << endl;
+    cout << setw(15) << "Ivanov" << setw(10) << "Sergey" << setw(15)
+         << "Osinovay 3" << setw(13) << "Nahodka" << endl;
+    cout << setw(15) << "Sidorov" << setw(10) << "Ivan" << setw(15)
+         << "Berezovaya 21" << setw(13) << "Kaliningrad" << endl;
     return 0;
 }
