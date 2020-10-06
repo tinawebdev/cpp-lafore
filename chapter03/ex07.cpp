dea8e3698cccfce07e1c2748101b49e6142eeac3
Kristina <codingkris@yahoo.com>
2020-10-06 09:26:08 +0300

    Completed exercise 07

---
+++ chapter03/ex07.cpp
@@ -0,0 +1,28 @@
+// Программа рассчитывает сумму денег, которые пользователь получит при
+// вложении начальной суммы с фксированной процентной ставкой дохода 
+// через определенное количество лет
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    double amount, percent, result;
+    unsigned int years = 1;
+    std::cout << "Введите начальный вклад: ";
+    std::cin >> amount;
+    std::cout << "Введите число лет: ";
+    std::cin >> years;
+    std::cout << "Введите процентную ставку: ";
+    std::cin >> percent;
+
+    for (int i = 0; i < years; i++) {
+        amount += amount*percent/100;
+        result = amount;
+        std::cout << result << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "Через " << years << " лет вы получите "
+              << result << " долларов" << std::endl;
+
+    return 0;
+}
